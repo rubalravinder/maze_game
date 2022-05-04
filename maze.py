6149fdf6c51581e2bdff6b3c6a6c7cfe8a62453b
GARRIGOS Martin <81553707+GARRIGOS-Martin@users.noreply.github.com>
2022-05-04 11:39:03 +0200

    MAZE v1 (#1)
    * terrain up
    
    * maze up
    
    * maze up
    
    * maze up
    
    * maze V1

---
+++ maze.py
@@ -0,0 +1,29 @@
+from re import X
+import numpy as np
+
+
+class Maze :
+
+    def __init__(self, h, w):
+        self.h = h
+        self.w = w
+        self.grid = np.full((h,w), -1)
+        self.grid[np.random.randint(0, max(h))][np.random.randint(0, max(w))] = 100
+        self.grid[np.random.randint(0, max(h))][np.random.randint(0, max(w))] = 100
+
+
+    def elegibility(self):
+        self.eligible_moves = np.full((10,10), 'UDLR')
+        self.eligible_moves[0] = 'DLR'
+        self.eligible_moves[:,0] = 'UDR'
+        self.eligible_moves[max(self.h)] = 'DLR'
+        self.eligible_moves[:,max(self.w)] = 'UDR'
+        
+    defhjhj
+
+
+
+
+
+
+    
