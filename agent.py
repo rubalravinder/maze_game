088ef87fb98b8b67824b6d8eb1d427793efa716c
Martin Garrigos <martin.garrigos38@gmail.com>
2022-05-04 17:05:40 +0200

    fin jour 2

--- agent.py
+++ agent.py
@@ -11,66 +11,112 @@ import matplotlib.pyplot as plt
 from maze import *
 from vizualizer import *
 
-class RandomAgent:
+class Agent:
 
     def __init__(self, maze):
         self.maze = maze
-        self.x = np.random.randint(maze.w)
-        self.y = np.random.randint(maze.h)
+        self.x = np.random.randint(0,maze.w)
+        self.y = np.random.randint(0,maze.h)
         self.rewards = []
 
-    def next_move(self):
+    def random_move(self, possible_moves_list):
+        return np.random.choice(possible_moves_list)
+
+    def get_possible_moves(self):
+        possible_moves = self.maze.eligible_moves[self.x][self.y]
+        return [c for c in possible_moves]
+
+    def next_move(self, move_func, moves):
         """
         Next move for the agent to do
         """
-        possible_moves = self.maze.eligible_moves[self.x][self.y]
-        possible_moves_list = [c for c in possible_moves]
-        move = np.random.choice(possible_moves_list)
+        
+        move = move_func(moves)
     
         if 'U' in move:
-            self.y -= 1
-        elif 'D' in move:
-            self.y += 1
-        elif 'L' in move:
             self.x -= 1
-        elif 'R' in move:
+        elif 'D' in move:
             self.x += 1
+        elif 'L' in move:
+            self.y -= 1
+        elif 'R' in move:
+            self.y += 1
     
     def get_reward(self):
         """
         Get reward for a (x,y) position
         """
         reward = self.maze.grid[self.x][self.y]
-        self.rewards.append(reward)
+        self.rewards[-1].append(reward)
         return reward
     
-    def episode(self):
+    def move(self):
         self.next_move()
         self.get_reward()
     
 
         
+    def new_position(self):
+        self.x = np.random.randint(0,maze.w)
+        self.y = np.random.randint(0,maze.h)
+
+class RandomAgent(Agent):
+    def __init__(self, maze):
+        super().__init__(maze)
+    
+    def next_move(self):
+        super().next_move(self.random_move)
+    
 
 
 
-class GreedyAgent:
+class GreedyAgent(Agent):
     
     def __init__(self, maze):
-        self.maze = maze
-        pass
+        super().__init__(maze)
+        self.gamma = 0.9
+        self.alpha = 0.1
+        self.epsilon = 0.2
+
+        # Initializing the Q-matrix
+        self.Q = np.full((self.maze.w, self.maze.h),fill_value = {'U' : 0, 'D' : 0, 'L': 0, 'R':0 })
+    
+
+    
+    def next_move(self):
+        d = np.random.random()
+        if self.epsilon < d :
+            max_index = np.argmax(self.Q[self.x, self.y].values())
+            move = list(self.Q[self.x, self.y].keys())[max_index]
+        else :
+            super().next_move(self.random_move)
+        self.update()
 
 
 
+    def update(state, state2, reward, action, action2):
+        predict = self.Q[state][action]
+        target = reward + gamma * Q[state2, action2]
+        Q[state, action] = Q[state, action] + alpha * (target - predict)
 
 if __name__ == '__main__' :
 
     maze = Maze(10,10)
-    dummy = RandomAgent(maze)
+    dummy = GreedyAgent(maze)
     visu = Visualizer(maze, dummy)
 
-    dummy.episode()
-    for i in range(5):
-        while dummy.rewards[-1] < 0:
+    nb_episode = 5
+    max_step   = 150
+
+    for i in range(nb_episode):
+        dummy.rewards.append([])
+
+        for it_step in range(max_step):
             print(dummy.x, dummy.y)
-            dummy.episode()
-        visu.plot_perf()
+            dummy.move()
+            if len(dummy.rewards[-1]) != 0 and dummy.rewards[-1][-1] > 0:
+                break
+
+        dummy.new_position()
+
+    visu.plot_perf()
--- vizualizer.py
+++ vizualizer.py
@@ -13,6 +13,9 @@ class Visualizer:
         Plot a 2D map representing the gain that were found.
         historic : DataFrame with columns x,y,gain for each trial.
         """
-        plt.plot(np.cumsum(self.agent.rewards), linestyle='', marker='*')
+        plt.figure(1)
+        for i in range(len(self.agent.rewards)):
+            plt.plot(np.cumsum(self.agent.rewards[i]), "+-", label="traj_{}".format(i))
         plt.grid()
+        plt.legend()
         plt.show()
