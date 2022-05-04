23a10fed00592df51f6578a16164a8f3ab7d8205
Martin Garrigos <martin.garrigos38@gmail.com>
2022-05-04 11:05:41 +0200

    terrain up

---
+++ maze.py
@@ -0,0 +1,14 @@
+from re import X
+import numpy as np
+
+
+class terrain :
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.grid = np.full((x,y), -1)
+        self.grid[4][6] = 100
+        self.grid[9][2] = 100
+
+    
