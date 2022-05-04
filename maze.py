1c5c420cf0e8969c86f64c9e18696edc84133906
Martin Garrigos <martin.garrigos38@gmail.com>
2022-05-04 11:46:44 +0200

    maze v2

--- maze.py
+++ maze.py
@@ -18,8 +18,12 @@ class Maze :
         self.eligible_moves[:,0] = 'UDR'
         self.eligible_moves[max(self.h)] = 'DLR'
         self.eligible_moves[:,max(self.w)] = 'UDR'
+        self.eligible_moves[0][0] = 'DR'
+        self.eligible_moves[0][max(self.w)] = 'DL'
+        self.eligible_moves[max(self.h)][max(self.w)] = 'UL'
+        self.eligible_moves[max(self.h)][0] = 'UR'
         
-    defhjhj
+    
 
 
 
